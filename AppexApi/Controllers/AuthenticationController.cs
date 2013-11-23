19fb1d08350bb6a3e80081706d764989642eb59c
fcmendoza <fcmendoza02@gmail.com>
2013-11-22 22:45:10 -0800

    Initial commit.

---
+++ AppexApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppexApi.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+        [HttpGet]
+        public User ValidateAndGetUser(string username, string password) {
+            // first validate the credentials are valid
+            bool isValid = true;
+
+            // return found user or 404 otherwise.
+            if (isValid) {
+                return new User { Username = username, IsActive = true, IaAdmin = false };
+            }
+            else {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound) {
+                    Content = new StringContent(string.Format("User with login '{0}' does not exist.", username)),
+                    ReasonPhrase = "User Not Found"
+                };
+                throw new HttpResponseException(response);
+            }
+        } 
+    }
+
+    public class User {
+        public string Username { get; set; }
+        public bool IsActive { get; set; }
+        public bool IaAdmin { get; set; }
+    }
+
+}
