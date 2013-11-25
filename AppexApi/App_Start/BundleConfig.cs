a439ba628b089504019bf51f854b9780485981e0
fcmendoza <fcmendoza02@gmail.com>
2013-11-24 18:56:02 -0800

    Moved pocket.js to a bundle.

--- AppexApi/App_Start/BundleConfig.cs
+++ AppexApi/App_Start/BundleConfig.cs
@@ -15,6 +15,9 @@ namespace AppexApi {
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/pocket").Include(
+                        "~/Scripts/pocket.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
