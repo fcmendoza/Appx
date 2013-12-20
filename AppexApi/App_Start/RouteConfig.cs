3cdb039149aef5205ad68525562306dff9341fad
fcmendoza <fcmendoza02@gmail.com>
2013-12-19 23:46:43 -0800

    Added custom route to map different url to the Links controller.

--- AppexApi/App_Start/RouteConfig.cs
+++ AppexApi/App_Start/RouteConfig.cs
@@ -10,6 +10,13 @@ namespace AppexApi {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Custom route. Should be added before the Default one
+            routes.MapRoute(
+                name: "Markdown",
+                url: "markdown", // use this until I decide the proper name of the controller.
+                defaults: new { controller = "links", action = "todo", style = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
