4647d826e059721c10c190b429df027a93ee62b9
fcmendoza <fcmendoza02@gmail.com>
2013-12-27 23:03:02 -0600

    Added route for todo page. Added new css styles for markdown.

--- AppexApi/App_Start/RouteConfig.cs
+++ AppexApi/App_Start/RouteConfig.cs
@@ -19,6 +19,13 @@ namespace AppexApi {
 
             // Custom route. Should be added before the Default one
             routes.MapRoute(
+                name: "Todos",
+                url: "todos",
+                defaults: new { controller = "Notes", action = "DisplayContent", filename = "todos", style = UrlParameter.Optional }
+            );
+
+            // Custom route. Should be added before the Default one
+            routes.MapRoute(
                 name: "Books",
                 url: "books",
                 defaults: new { controller = "Notes", action = "DisplayContent", filename = "books", style = UrlParameter.Optional }
