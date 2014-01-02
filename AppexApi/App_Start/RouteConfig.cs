ef26139c30f2e57d1cc6b9c92ea07741c6e4e8c6
fcmendoza <fcmendoza02@gmail.com>
2014-01-02 16:17:39 -0600

    Added custom route based on host name.

--- AppexApi/App_Start/RouteConfig.cs
+++ AppexApi/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,11 @@ namespace AppexApi {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // Custom route. Should be added before the Default one
-            routes.MapRoute(
-                name: "Notes",
-                url: "notes/{filename}",
-                defaults: new { controller = "Notes", action = "DisplayContent", style = UrlParameter.Optional }
-            );
+            bool redirectionEnabled = bool.TryParse(ConfigurationManager.AppSettings["CustomUrlRedirectionEnabled"], out redirectionEnabled) ? redirectionEnabled : false;
+
+            if (redirectionEnabled) {
+                routes.Add(new SubdomainRoute());
+            }
 
             // Custom route. Should be added before the Default one
             routes.MapRoute(
@@ -50,6 +50,54 @@ namespace AppexApi {
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+        }
+    }
+
+    public class SubdomainRoute : RouteBase {
+
+        public override RouteData GetRouteData(HttpContextBase httpContext) {
+            //var url = httpContext.Request.Headers["HOST"];
+            //var index = url.IndexOf(".");
+
+            //if (index < 0)
+            //    return null;
+
+            //var subDomain = url.Substring(0, index);
+
+            //if (subDomain == "user1") {
+            //    var routeData = new RouteData(this, new MvcRouteHandler());
+            //    routeData.Values.Add("controller", "User1"); //Goes to the User1Controller class
+            //    routeData.Values.Add("action", "Index"); //Goes to the Index action on the User1Controller
+
+            //    return routeData;
+            //}
+
+            //if (subDomain == "user2") {
+            //    var routeData = new RouteData(this, new MvcRouteHandler());
+            //    routeData.Values.Add("controller", "User2"); //Goes to the User2Controller class
+            //    routeData.Values.Add("action", "Index"); //Goes to the Index action on the User2Controller
+
+            //    return routeData;
+            //}
+
+            string hostname = httpContext.Request.Url.Host;
+            string value = ConfigurationManager.AppSettings[hostname] ?? null;
+
+            if (value != null) {
+                var routeData = new RouteData(this, new MvcRouteHandler());
+                routeData.Values.Add("controller", "Notes"); //Goes to the NotesController class
+                routeData.Values.Add("action", "DisplayContent"); //Goes to the DisplayContent action on the NotesController
+                routeData.Values.Add("filename", value); // sets the filename parameter to the specified in the web.config
+
+                return routeData;
+            }
+
+            return null;
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values) {
+            //Implement your formating Url formating here
+            return null;
         }
     }
 }
