29cf20bb9277e36cc17191336b4656137a64b674
fcmendoza <fcmendoza02@gmail.com>
2014-01-02 22:40:09 -0600

    Fixed routing of default page (default Markdown file actually) based on domain name.

--- AppexApi/App_Start/RouteConfig.cs
+++ AppexApi/App_Start/RouteConfig.cs
@@ -11,93 +11,43 @@ namespace AppexApi {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            bool redirectionEnabled = bool.TryParse(ConfigurationManager.AppSettings["CustomUrlRedirectionEnabled"], out redirectionEnabled) ? redirectionEnabled : false;
+            // Custom routes should be added before the Default one.
+             routes.MapRoute(
+                 name: "Notes",
+                 url: "notes/{filename}",
+                 defaults: new { controller = "Notes", action = "DisplayContent", style = UrlParameter.Optional }
+             );
 
-            if (redirectionEnabled) {
-                routes.Add(new SubdomainRoute());
-            }
-
-            // Custom route. Should be added before the Default one
             routes.MapRoute(
                 name: "Moni",
                 url: "monica",
                 defaults: new { controller = "Notes", action = "DisplayContent", filename = "monica", style = UrlParameter.Optional }
             );
 
-            // Custom route. Should be added before the Default one
             routes.MapRoute(
                 name: "Todos",
                 url: "todos",
                 defaults: new { controller = "Notes", action = "DisplayContent", filename = "todos", style = UrlParameter.Optional }
             );
 
-            // Custom route. Should be added before the Default one
             routes.MapRoute(
                 name: "Books",
                 url: "books",
                 defaults: new { controller = "Notes", action = "DisplayContent", filename = "books", style = UrlParameter.Optional }
             );
 
-            // Custom route. Should be added before the Default one
             routes.MapRoute(
                 name: "Markdown",
                 url: "markdown",
                 defaults: new { controller = "Notes", action = "DisplayContent", filename="markdown-test", style = UrlParameter.Optional }
             );
 
+            // Default route
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-        }
-    }
-
-    public class SubdomainRoute : RouteBase {
-
-        public override RouteData GetRouteData(HttpContextBase httpContext) {
-            //var url = httpContext.Request.Headers["HOST"];
-            //var index = url.IndexOf(".");
-
-            //if (index < 0)
-            //    return null;
-
-            //var subDomain = url.Substring(0, index);
-
-            //if (subDomain == "user1") {
-            //    var routeData = new RouteData(this, new MvcRouteHandler());
-            //    routeData.Values.Add("controller", "User1"); //Goes to the User1Controller class
-            //    routeData.Values.Add("action", "Index"); //Goes to the Index action on the User1Controller
-
-            //    return routeData;
-            //}
-
-            //if (subDomain == "user2") {
-            //    var routeData = new RouteData(this, new MvcRouteHandler());
-            //    routeData.Values.Add("controller", "User2"); //Goes to the User2Controller class
-            //    routeData.Values.Add("action", "Index"); //Goes to the Index action on the User2Controller
-
-            //    return routeData;
-            //}
-
-            string hostname = httpContext.Request.Url.Host;
-            string value = ConfigurationManager.AppSettings[hostname] ?? null;
-
-            if (value != null) {
-                var routeData = new RouteData(this, new MvcRouteHandler());
-                routeData.Values.Add("controller", "Notes"); //Goes to the NotesController class
-                routeData.Values.Add("action", "DisplayContent"); //Goes to the DisplayContent action on the NotesController
-                routeData.Values.Add("filename", value); // sets the filename parameter to the specified in the web.config
-
-                return routeData;
-            }
-
-            return null;
-        }
-
-        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values) {
-            //Implement your formating Url formating here
-            return null;
         }
     }
 }
--- AppexApi/Controllers/HomeController.cs
+++ AppexApi/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace AppexApi.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() {
+        public ActionResult Index(string style) {
+            bool redirectionEnabled = bool.TryParse(ConfigurationManager.AppSettings["CustomUrlRedirectionEnabled"], out redirectionEnabled) ? redirectionEnabled : false;
+            
+            if (redirectionEnabled) {
+                string hostname = HttpContext.Request.Url.Host;
+                string filename = ConfigurationManager.AppSettings[hostname] ?? null;
+
+                if (filename != null) {
+                    string htmlText = new Api.LinksController().GetHttmlFromMarkdown(url: String.Format("https://dl.dropboxusercontent.com/u/26506865/{0}.txt", filename));
+
+                    ViewBag.Style = style;
+                    ViewBag.Title = filename;
+                    return View("../Notes/Content", new MarkdownViewModel { Body = htmlText });
+                }
+            }
+
             return View();
         }
     }
